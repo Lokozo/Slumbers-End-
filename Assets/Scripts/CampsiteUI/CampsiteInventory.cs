97c2c60c1e653ccd0a37e55af9496479b5cafbf4
Lokozo <jeanandree11@gmail.com>
2026-04-30 22:41:22 +0800

    34
    mugana na ang custcene

---
+++ Assets/Scripts/CampsiteUI/CampsiteInventory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CampsiteInventory : MonoBehaviour
+{
+    public static CampsiteInventory Instance;
+
+    public System.Action OnInventoryChanged; // ✅ ADD THIS
+
+    private Dictionary<Item, int> inventory = new Dictionary<Item, int>();
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddItem(Item item, int amount)
+    {
+        if (inventory.ContainsKey(item))
+            inventory[item] += amount;
+        else
+            inventory[item] = amount;
+
+        Debug.Log($"[CampsiteInventory] Added {amount}x {item.itemName}");
+
+        OnInventoryChanged?.Invoke(); // ✅ CALL EVENT
+    }
+
+    public void RemoveItem(Item item, int amount)
+    {
+        if (!inventory.ContainsKey(item)) return;
+
+        inventory[item] -= amount;
+
+        if (inventory[item] <= 0)
+            inventory.Remove(item);
+
+        OnInventoryChanged?.Invoke(); // ✅ CALL EVENT
+    }
+
+    public Dictionary<Item, int> GetInventory()
+    {
+        return inventory;
+    }
+}
