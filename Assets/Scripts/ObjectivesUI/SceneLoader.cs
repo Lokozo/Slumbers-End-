abf075aa7800eeca0929c34604f6956a31f52a14
AlicaLynn17 <marcelinetabada@gmail.com>
2026-05-01 19:39:10 +0800

    marcy commit
    cutscenes from chap 1 to 6

--- Assets/Scripts/ObjectivesUI/SceneLoader.cs
+++ Assets/Scripts/ObjectivesUI/SceneLoader.cs
@@ -4,36 +4,44 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
-            //SceneManager.LoadScene("Chapter 2");
-            StartCoroutine(LoadSceneAsyncRoutine("Chapter 2"));
-
-
+            LoadNextScene();
         }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine(LoadSceneAsyncRoutine("Chapter 2"));
-            //SceneManager.LoadScene("Chapter 2");
-            // Optionally disable this trigger so it doesn't retrigger
-            //gameObject.SetActive(false);
+            LoadNextScene();
         }
     }
 
-    private IEnumerator LoadSceneAsyncRoutine(string sceneName)
+    void LoadNextScene()
     {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = currentIndex + 1;
+
+        // Prevent going out of bounds
+        if (nextIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            StartCoroutine(LoadSceneAsyncRoutine(nextIndex));
+        }
+        else
+        {
+            Debug.Log("No more scenes to load.");
+        }
+    }
+
+    private IEnumerator LoadSceneAsyncRoutine(int sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
 
         while (!asyncLoad.isDone)
         {
-            // Optional: You can show loading progress here via asyncLoad.progress
             yield return null;
         }
     }
