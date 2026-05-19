9ce7365e0b6a5bea4525a649cf9c60a3d307bdeb
AlicaLynn17 <marcelinetabada@gmail.com>
2026-05-19 14:11:31 +0800

    marcy commit

--- Assets/Scripts/InteractTrigger.cs
+++ Assets/Scripts/InteractTrigger.cs
@@ -7,6 +7,11 @@ public class InteractTrigger : MonoBehaviour
         if (!other.CompareTag("Player")) return;
 
         UIManager.Instance.ladderHoldEIcon.SetActive(true);
+
+        TutorialUIManager.Instance.ShowStep(
+            "LadderTutorial",
+            "Hold E to grab the ladder\nPress W or S to climb"
+            );
     }
     
     private void OnTriggerExit(Collider other)
