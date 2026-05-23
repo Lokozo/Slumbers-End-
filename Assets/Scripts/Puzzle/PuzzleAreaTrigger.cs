931524d12e078e8e9d49eda98d4c2b25ea9e73f4
Lokozo <jeanandree11@gmail.com>
2026-05-23 23:58:26 +0800

    Puzzles
    puzzles

---
+++ Assets/Scripts/Puzzle/PuzzleAreaTrigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PuzzleAreaTrigger : MonoBehaviour
+{
+    public Item requiredNote;
+
+    [Header("Sound")]
+    public AudioClip activationSound;
+    public AudioSource audioSource;
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggered)
+            return;
+
+        if (!other.CompareTag("Player"))
+            return;
+
+        // PLAYER DOESN'T HAVE NOTE
+        if (!PlayerInventory.Instance.HasItem(requiredNote))
+        {
+            Debug.Log("Missing required note!");
+            return;
+        }
+
+        triggered = true;
+
+        Debug.Log("Correct area entered!");
+
+        PuzzleManager.Instance.enteredCorrectArea = true;
+
+        // 🔥 PLAY ACTIVATION SOUND
+        if (audioSource != null && activationSound != null)
+        {
+            audioSource.PlayOneShot(activationSound);
+        }
+
+        PuzzleManager.Instance.CheckPuzzleComplete();
+    }
+}
